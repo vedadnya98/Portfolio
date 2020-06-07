ddf0189cc37bb2c9f2bd9e435a2f7be437ccdbc8
Vedadnya Jadhav <vedadnya@gmail.com>
2020-06-06 21:25:34 -0400

    Updates

---
+++ precache-manifest.2b65d1e12fc489f9ae188e661338bea6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb8c45f3a293dab27e8873e5e7e7533",
+    "url": "/vedadnya-jadhav-portfolio/index.html"
+  },
+  {
+    "revision": "4dbf1663621c9cd94c60",
+    "url": "/vedadnya-jadhav-portfolio/static/css/main.4d290c35.chunk.css"
+  },
+  {
+    "revision": "5805fcaa7587a22a36c8",
+    "url": "/vedadnya-jadhav-portfolio/static/js/2.d6e3932e.chunk.js"
+  },
+  {
+    "revision": "4e2c02fcb88b596eaa92de7b97a0e735",
+    "url": "/vedadnya-jadhav-portfolio/static/js/2.d6e3932e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa692d90f96339fc10ab",
+    "url": "/vedadnya-jadhav-portfolio/static/js/3.615596b7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/vedadnya-jadhav-portfolio/static/js/3.615596b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c8da18206d688d2a8b",
+    "url": "/vedadnya-jadhav-portfolio/static/js/4.1d624753.chunk.js"
+  },
+  {
+    "revision": "39391d0d2da6a212ce75",
+    "url": "/vedadnya-jadhav-portfolio/static/js/5.65ced9d3.chunk.js"
+  },
+  {
+    "revision": "4dbf1663621c9cd94c60",
+    "url": "/vedadnya-jadhav-portfolio/static/js/main.566581b4.chunk.js"
+  },
+  {
+    "revision": "25cc55d62ec0096df0c6",
+    "url": "/vedadnya-jadhav-portfolio/static/js/runtime-main.c6696f54.js"
+  },
+  {
+    "revision": "f3c94bcea82a053ce7061a6b94fc648b",
+    "url": "/vedadnya-jadhav-portfolio/static/media/Resume.f3c94bce.pdf"
+  },
+  {
+    "revision": "4391d1a2f80872d1172a105d448f3ed2",
+    "url": "/vedadnya-jadhav-portfolio/static/media/mail.4391d1a2.svg"
+  },
+  {
+    "revision": "947cde4a2dcf526cea45c37294ba1208",
+    "url": "/vedadnya-jadhav-portfolio/static/media/profileBeardLight.947cde4a.svg"
+  },
+  {
+    "revision": "1b6c1804f5356985d67906ae2aff1f07",
+    "url": "/vedadnya-jadhav-portfolio/static/media/profileLight.1b6c1804.svg"
+  },
+  {
+    "revision": "50b8f6455594053ea624b09c953f0b3b",
+    "url": "/vedadnya-jadhav-portfolio/static/media/vectorLight.50b8f645.png"
+  }
+]);
